256bde7160c71ffe012131a88a4aecc2ad7e3897
Administrator <parastoo.sa.1982@gmail.com>
2024-04-16 12:50:34 -0400

    changes

--- src/main/java/com/selfstudy/accounts/entity/BaseEntity.java
+++ src/main/java/com/selfstudy/accounts/entity/BaseEntity.java
@@ -2,21 +2,29 @@ package com.selfstudy.accounts.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import java.util.Date;
 
-import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter @Setter @ToString
 public class BaseEntity {
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(updatable = false)
-    private LocalDateTime createdAt;
+    private Date createdAt;
     @Column(updatable = false)
     private String CreatedBy;
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(updatable = false)
-    private LocalDateTime updatedAt;
+    private Date updatedAt;
     @Column(updatable = false)
     private String updatedBy;
 }
