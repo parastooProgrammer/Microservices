54832d11c9a3c966ef0940b3e4f1b8e72407afce
Administrator <parastoo.sa.1982@gmail.com>
2024-04-19 01:10:04 -0400

    handle exception

--- src/main/java/com/selfstudy/accounts/controller/AccountController.java
+++ src/main/java/com/selfstudy/accounts/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.selfstudy.accounts.controller;
 import com.selfstudy.accounts.constants.AccountConstant;
 import com.selfstudy.accounts.dto.CustomerDto;
 import com.selfstudy.accounts.dto.ResponseDto;
+import com.selfstudy.accounts.service.IAccountService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountController {
+
+    private IAccountService iAccountService;
 
     @GetMapping("sayHello")
     public String sayHello() {
@@ -22,7 +27,7 @@ public class AccountController {
 
     @PostMapping("/craete")
     public ResponseEntity<ResponseDto> createAccount(CustomerDto customerDto) {
-
+        iAccountService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountConstant.STATUS_201,
                         AccountConstant.MESSAGE_201));
