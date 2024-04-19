da3543f4c10fddaf41dc007433800d8bf261bd44
Administrator <parastoo.sa.1982@gmail.com>
2024-04-18 22:22:38 -0400

    handle exception

--- src/main/java/com/selfstudy/accounts/service/imp/AccountServiceImp.java
+++ src/main/java/com/selfstudy/accounts/service/imp/AccountServiceImp.java
@@ -4,6 +4,7 @@ import com.selfstudy.accounts.constants.AccountConstant;
 import com.selfstudy.accounts.dto.CustomerDto;
 import com.selfstudy.accounts.entity.Account;
 import com.selfstudy.accounts.entity.Customer;
+import com.selfstudy.accounts.exception.CustomerAlreadyExistException;
 import com.selfstudy.accounts.mapper.CustomerMapper;
 import com.selfstudy.accounts.repository.AccountRepository;
 import com.selfstudy.accounts.repository.CustomerRepository;
@@ -11,6 +12,7 @@ import com.selfstudy.accounts.service.IAccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -26,6 +28,11 @@ public class AccountServiceImp implements IAccountService {
     @Override
     public void createAccount(CustomerDto customerDto) {
         Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByCustomerMobileNumber(customerDto.getMobileNumber());
+        if (optionalCustomer.isPresent()) {
+           throw new CustomerAlreadyExistException("Customer Already Exists with The given Mobile Number "
+                   +customerDto.getMobileNumber());
+        }
         Customer savedCustomer = customerRepository.save(customer);
         Account account = createNewAccount(savedCustomer);
 
