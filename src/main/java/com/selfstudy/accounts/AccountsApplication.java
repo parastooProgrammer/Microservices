a7bdef51e7a82ba24a550d1f8d28d9059257fc57
Administrator <parastoo.sa.1982@gmail.com>
2024-05-14 00:02:01 -0400

    swager

--- src/main/java/com/selfstudy/accounts/AccountsApplication.java
+++ src/main/java/com/selfstudy/accounts/AccountsApplication.java
@@ -1,11 +1,26 @@
 package com.selfstudy.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(info = @Info(title = "Account Microservice Rest Api Documentation",
+		description = "Selfstudy Account Microservice Rest Api Documentation",
+		version = "v1",
+		contact = @Contact(name = "Parastoo"),
+		license = @License(name = "Apache 2.0")),
+		externalDocs = @ExternalDocumentation(description="xxxxxxx")
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
